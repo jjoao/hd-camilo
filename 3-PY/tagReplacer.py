d2c7bc96920202265a3942f9740d0e982ebc83e0
dhumanity <115653163+dhumanity@users.noreply.github.com>
2022-12-12 19:30:33 +0000

    module to replace/remove SGML tags
    To-Do:
    	[ ] further testing
    	[ ] make it receive `stdin`
    
    Currently working for:
    	[✔︎] tags w/o attributes
    	[✔︎] tags w attributes
    	[✔︎] replacing self_closing tags
    	[✔︎] removing self_closing tags
    
    Currently misbehaving for:
    
    Currently CRASHING for:

---
+++ 3-PY/tagReplacer.py
@@ -0,0 +1,59 @@
+import re, sys, doctest
+
+def tagReplacer(text, tag='', repl='', self_closing=False):
+
+	"""
+	Given a text, replace/strip tag
+    _defaults to stripping all tags
+
+	:param text: str
+	:param tag:  str
+	:param repl: str
+	:param self_closing: boolean
+	:return: str
+
+	>>> tagReplacer('O gato <b>preto</b> cruzou a estrada<br />')
+	'O gato preto cruzou a estrada'
+	>>> tagReplacer('O gato <b>preto</b> cruzou a estrada<br />','b')
+	'O gato preto cruzou a estrada<br />'
+	>>> tagReplacer('O gato <b>preto</b> cruzou a estrada<br />','b','emph')
+	'O gato <emph>preto</emph> cruzou a estrada<br />'
+	>>> tagReplacer("O gato <b>preto</b> cruzou a estrada<br />","b","emph rend='bold'")
+	"O gato <emph rend='bold'>preto</emph> cruzou a estrada<br />"
+	>>> tagReplacer('O gato <b>preto</b> cruzou a estrada<br />','br',self_closing=True)
+	'O gato <b>preto</b> cruzou a estrada'
+	>>> tagReplacer('O gato <b>preto</b> cruzou a estrada<br />','br','lb', True)
+	'O gato <b>preto</b> cruzou a estrada<lb />'
+	>>> tagReplacer('O gato <b>preto</b> cruzou a estrada<br />','br','lb class="dummy"', True)
+	'O gato <b>preto</b> cruzou a estrada<lb class="dummy" />'
+
+	>>> tagReplacer('<span class="fr">Le chat est monté sur le toit.</span>','span class="fr"')
+	'Le chat est monté sur le toit.'
+	"""
+
+	t_closer = (r'</'+re.sub(r'(^\w+)(?:.*)', r'\1', tag )+'>' 				if tag  != '' else '')  if not self_closing else ''
+	pattern  = ('<'+tag+r'(?:(?: ?>)|(?: +[^\w]+[^/>]*?>))(.*?)'+t_closer)  if not self_closing else r'<'+tag+r' ?[^\\]*?>'
+	r_opener = (r'<'+repl+r'>'  if not self_closing else r'<'+repl+r' />') 	if repl != '' else ''
+	r_closer = (r'</'+re.sub(r'(^\w+)(?:.*)', r'\1', repl)+'>' 				if not self_closing else '') if repl != '' else ''
+	replace  = (r_opener+r'\1'+r_closer            							if not self_closing else r'<'+repl+r' />') if repl != '' else ''
+
+	# print(f'tag={tag}\n{t_closer}')
+	# print(f'repl={repl}\n{r_opener}\n{r_closer}')
+	# print(f'pattern={pattern}\nreplace={replace}')
+
+	if tag == '':
+		strip_all_tags = re.sub(r'<[^>]*?>', r'', text)
+		output = strip_all_tags
+	elif repl == '':
+		strip_select_tag = re.sub(pattern, r'\1', text) if not self_closing else re.sub(pattern, '', text)
+		output = strip_select_tag
+	else:
+		replace_select_tag = re.sub(pattern, replace, text)
+		output = replace_select_tag
+
+	return output
+
+doctest.testmod()
+
+# if __name__ == '__main__':
+#     print("untagged text bellow:\n\n",tagReplacer(sys.stdin.read()))
