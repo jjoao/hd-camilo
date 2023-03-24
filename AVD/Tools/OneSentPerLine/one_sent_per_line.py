e2961feb784dcdd118266bb1184dafe4cf4de2d5
jjoao <jj@di.uminho.pt>
2023-03-24 13:00:23 +0000

    one sentence per line   pip install .   onesentpl --help

---
+++ AVD/Tools/OneSentPerLine/one_sent_per_line.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''
+NAME
+   onesentpl - convert into oner-sentence-per-line format
+
+Usage:
+   onesentpl file.txt > nova.txt
+
+'''
+__version__ = "0.0.1"
+
+from jjcli import * 
+
+def main():
+    cl=clfilter(opt="l", doc=__doc__) 
+    abrev = r'((Sr|Sra|Exm[oa]?|Ilm[oa]|Dr|Dra)\.)\n'
+    
+    for par in cl.paragraph():      ## process paragrafo de cada vez
+        if match(r'---\s*\n',par):  ## não processa as linhas "---" metadados
+            print(par, "\n")
+            continue
+        par = re.sub(r'(#.*)', r'\1§', par)                 ## titulos markdown
+        par = re.sub(r'(\w)-\n *([a-zç-])',  r'\1\2', par); ## translineações
+        par = re.sub(r'\n', r' ', par)
+        par = re.sub(r'([a-zãáéíúó0-9][.?!]+) ([A-ZÁÉÍÓÚ\-])', r'\1\n\2', par)
+        par = re.sub(r'([a-zãáéíúó0-9][.?!]+) (— *[A-ZÁÉÍÓÚ])', r'\1\n\2', par)
+        par = re.sub(r' +', r' ', par) 
+        par = re.sub(abrev, r'\1 ', par)                    ## abreviaturas
+        par = re.sub(r'(#.*)§', r'\n\1\n', par)
+        if "-l" in cl.opt:
+            par = re.sub(r'—', r'-', par)
+    
+        print (par, "\n")
+
+if __name__ == "__main__":
+    main()
