15a7f10f9a7bf631827e086c2d8fcc816bbaef42
jjoao <jj@di.uminho.pt>
2023-03-31 23:12:06 +0100

    typo

--- AVD/Aula5/getNP-oco.py
+++ AVD/Aula5/getNP-oco.py
@@ -3,8 +3,9 @@
 from jjcli import *
 from collections import Counter
 
-cl = clfilter("")
+cl = clfilter("n:")
 oco = Counter()
+q = int(cl.opt["-n"]) if "-n" in cl.opt else 20
 
 for linha in cl.input():
     if "NP" in linha:
@@ -12,7 +13,7 @@ for linha in cl.input():
         oco[word] = oco[word]+1
         ### oco[word] += 1
 
-for  w,n  in  oco.most_common(30):
+for  w,n  in  oco.most_common(q):
     print( f"{w},{n}")
 
 #print(oco.most_common(30),"\n")
